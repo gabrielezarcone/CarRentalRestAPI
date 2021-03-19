afdf6b1ba0efc97c32e69308f56b75ae964b3d01
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-19 12:24:07 +0100

    initial commit

---
+++ src/test/java/com/zarconeg/carRentalRestApi/CarRentalRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarconeg.carRentalRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
