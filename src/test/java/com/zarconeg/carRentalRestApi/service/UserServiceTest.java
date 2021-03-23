e187e5f9c74884384ee8f6a1e89fd6517e39bc05
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-23 12:05:18 +0100

    Altri test per i metodi di UserService

--- src/test/java/com/zarconeg/carRentalRestApi/service/UserServiceTest.java
+++ src/test/java/com/zarconeg/carRentalRestApi/service/UserServiceTest.java
@@ -4,7 +4,7 @@ import com.zarconeg.carRentalRestApi.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,6 +14,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
 import java.util.Date;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
@@ -35,5 +38,71 @@ class UserServiceTest {
         // -------------------------------
         User user1 = userService.save(user);
         assertEquals(user,user1);
+    }
+
+    @Test
+    @DisplayName("Trova utente by ID")
+    void findUserById(){
+        User user = new User();
+        user.setUsername("test");
+        user.setBirthDate(new Date());
+        user.setEmail("test@test.com");
+        user.setPassword("testPassword");
+        user.setName("Test");
+        user.setSurname("Test2");
+        // -------------------------------
+        User user1 = userService.save(user);
+        // -------------------------------
+        Optional<User> userOpt = userService.findById(user1.getId());
+        assertTrue(userOpt.isPresent());
+        userOpt.ifPresent( userObj -> {
+            assertEquals(userObj.getUsername(), "test");
+            assertEquals(userObj.getEmail(), "test@test.com");
+            assertEquals(userObj.getPassword(), "testPassword");
+        } );
+    }
+
+    @Test
+    @DisplayName("Elimina utente")
+    void deleteUser(){
+        User user = new User();
+        user.setUsername("test");
+        user.setBirthDate(new Date());
+        user.setEmail("test@test.com");
+        user.setPassword("testPassword");
+        user.setName("Test");
+        user.setSurname("Test2");
+        // -------------------------------
+        User user1 = userService.save(user);
+        // -------------------------------
+        Optional<User> userOpt = userService.findById(user1.getId());
+        assertTrue(userOpt.isPresent());
+        // -------------------------------
+        userOpt.ifPresent( userObj -> userService.delete(userObj));
+        // -------------------------------
+        userOpt = userService.findById(user1.getId());
+        assertFalse(userOpt.isPresent());
+    }
+
+    @Test
+    @DisplayName("Elimina utente by ID")
+    void deleteUserById(){
+        User user = new User();
+        user.setUsername("test");
+        user.setBirthDate(new Date());
+        user.setEmail("test@test.com");
+        user.setPassword("testPassword");
+        user.setName("Test");
+        user.setSurname("Test2");
+        // -------------------------------
+        User user1 = userService.save(user);
+        // -------------------------------
+        Optional<User> userOpt = userService.findById(user1.getId());
+        assertTrue(userOpt.isPresent());
+        // -------------------------------
+        userOpt.ifPresent( userObj -> userService.deleteById(userObj.getId()));
+        // -------------------------------
+        userOpt = userService.findById(user1.getId());
+        assertFalse(userOpt.isPresent());
     }
 }
