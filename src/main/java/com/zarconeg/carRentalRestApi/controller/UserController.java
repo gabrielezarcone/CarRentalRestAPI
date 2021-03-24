7c4d053fbcdd6b4394a0b54611dcc7523c319f5e
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-24 12:14:36 +0100

    POST /api/user crea un nuovo utente

---
+++ src/main/java/com/zarconeg/carRentalRestApi/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.zarconeg.carRentalRestApi.controller;
+
+import com.zarconeg.carRentalRestApi.domain.User;
+import com.zarconeg.carRentalRestApi.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
+     // POST /api/user
+     // Crea un nuovo utente
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<?> newUser(@RequestBody User newUser, UriComponentsBuilder ucBuilder){
+        userService.save(newUser);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/api/user/{id}").buildAndExpand(newUser.getId()).toUri());
+        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+    }
+}
