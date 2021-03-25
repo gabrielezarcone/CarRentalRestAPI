187035ab112ff0fef490684de38fe6e235a82670
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-25 09:36:36 +0100

    Aggiunge log di errore se problemi di integriità

--- src/main/java/com/zarconeg/carRentalRestApi/exception/restHandler/RestExeptionsHandler.java
+++ src/main/java/com/zarconeg/carRentalRestApi/exception/restHandler/RestExeptionsHandler.java
@@ -1,6 +1,8 @@
 package com.zarconeg.carRentalRestApi.exception.restHandler;
 
 import com.zarconeg.carRentalRestApi.exception.UserIntegrityException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,16 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 public class RestExeptionsHandler extends ResponseEntityExceptionHandler {
 
+    public final Logger LOG = LoggerFactory.getLogger(RestExeptionsHandler.class);
+
     @ExceptionHandler(UserIntegrityException.class)
     public final ResponseEntity<ErrorResponse> userIntegrity(Exception ex){
         ErrorResponse error = new ErrorResponse();
         error.setMessage(ex.getMessage());
         error.setStatus(HttpStatus.BAD_REQUEST);
+        // ----------------------------------------
+        LOG.error(error.getMessage());
+        // ----------------------------------------
         return new ResponseEntity<>(error, new HttpHeaders(), error.getStatus());
     }
 
