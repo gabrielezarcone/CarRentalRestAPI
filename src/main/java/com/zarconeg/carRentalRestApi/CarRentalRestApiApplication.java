40c055b8a79d4306e1263cff0ac14b36d35a2acf
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-26 17:00:45 +0100

    BCryptPasswordEncoder

--- src/main/java/com/zarconeg/carRentalRestApi/CarRentalRestApiApplication.java
+++ src/main/java/com/zarconeg/carRentalRestApi/CarRentalRestApiApplication.java
@@ -2,6 +2,8 @@ package com.zarconeg.carRentalRestApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CarRentalRestApiApplication {
@@ -10,4 +12,8 @@ public class CarRentalRestApiApplication {
 		SpringApplication.run(CarRentalRestApiApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 }
